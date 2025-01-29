d1f7baec6a5cc36258610fc46c979853e87acbfa
samuel olowoyeye <samuelolowoyeye38@gmail.com>
2025-01-29 13:52:26 +0100

    add changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -178,7 +178,7 @@ export default function Navbar() {
               closeMenu();
               openModal();
             }}
-            className="block w-full text-center mb-6 py-3 mt-2 bg-orange-500 hover:bg-orange-400 text-white rounded-md"
+            className="block mx-2 text-center px-40 my-2.5 py-2.5 bg-orange-500 hover:bg-orange-400 text-white rounded-xl"
           >
             Log in
           </button>
